a738c0aa4dcef842f45b3ae1f01a8b09845070b2
Chris <chris.reviresco@gmail.com>
2020-06-05 16:21:43 +0200

    add to game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,7 +1,16 @@
 // this creates a function named fight
+var playerName = window.prompt("What is your robot's name?");
+var name = "your name";
+// Note the lack of quotationn marks around playerName
+console.log(name);
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
 
 function fight() {
   window.alert("This fight has begun!");
 }
 
-fight();
+//fight();
